61a957bdbca376cce555eeb418a3f8b38db78072
il and jf <j.k.fokes@gmail.com>
2016-06-06 15:11:56 -0700

    More Stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,30 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-    rentals: this.store.findAll('rental'),
-    announcements: this.store.findAll('announcement')
+     rentals: this.store.findAll('rental'),
+     announcements: this.store.findAll('announcement')
     });
-  },
-  
-  actions: {
+   },
+   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
