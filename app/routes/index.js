83146ef39c0a21003e3c9d0c0c0381374d48007d
il and jf <ilesikar@gmail.com>
2016-06-06 14:20:39 -0700

    Stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+    rentals: this.store.findAll('rental'),
+    announcements: this.store.findAll('announcement')
+    });
   },
-
+  
   actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
       this.transitionTo('index');
     },
 
@@ -18,3 +31,6 @@ export default Ember.Route.extend({
     }
   }
 });
+
+
+//The RSVP.hash returns a new promise that is only fulfilled when all promises it contains are fulfilled
