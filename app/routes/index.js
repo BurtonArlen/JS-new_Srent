fcaeedbcdc2b8a623e44cb814a006f0cf6e82d07
Arlen <burton.arlen.1990@gmail.com>
2016-06-09 16:40:59 -0700

    finished lessons, firebase has bugged request links

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,13 +4,23 @@ export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
      rentals: this.store.findAll('rental'),
+     cities: this.store.findAll('city'),
      announcements: this.store.findAll('announcement')
     });
    },
+
    actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      var newCity = this.store.createRecord('city', params);
+      newCity.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      city.destroyRecord();
       this.transitionTo('index');
     }
   }
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://rental-app-c11bd.firebaseio.com/',
+    firebase: 'https://super-rentals-14d79.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
